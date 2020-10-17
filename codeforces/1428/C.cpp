b18f45ba5a75dd699b71a82190e487aecd2e8e8f
Kalash Shah <kalashshah11@gmail.com>
2020-10-17 18:56:00 +0530

    Add solution for problem `C - ABBB` Link: https://codeforces.com/contest/1428/problem/C Tags: brute force, data structures, greedy, strings, *1100 Ref: https://codeforces.com/contest/1428/submission/95755142

---
+++ codeforces/1428/C.cpp
@@ -0,0 +1,129 @@
+
+// Problem: C. ABBB
+// Contest: Codeforces - Codeforces Raif Round 1 (Div. 1 + Div. 2)
+// URL: https://codeforces.com/contest/1428/problem/C
+// Memory Limit: 256 MB
+// Time Limit: 1000 ms
+// Powered by CP Editor (https://github.com/cpeditor/cpeditor)
+
+#include <bits/stdc++.h>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstring>
+#include <chrono>
+#include <complex>
+#define REP(i,a,b) for (auto i = a; i != b; i++)
+#define ll long long int
+#define ld long double
+#define vi vector<int>
+#define vll vector<ll>
+#define vvi vector < vi >
+#define pii pair<int,int>
+#define pll pair<long long, long long>
+#define mod 1000000007
+#define inf 1000000000000000001
+#define all(c) c.begin(),c.end()
+#define mp(x,y) make_pair(x,y)
+#define mem(a,val) memset(a,val,sizeof(a))
+#define eb emplace_back
+#define f first
+#define s second
+#define pb push_back
+#define SQ(a) (a)*(a)
+ 
+using namespace std;
+
+void read(int n,vector<int>& x)
+{
+    x.clear();
+    x.resize(n);
+    for(int i = 0;i<n;i++)
+    {
+        cin>>x[i];
+    }
+    
+}
+void read(int n,int m,vector<vector<int>>& x)
+{
+    x.clear();
+    x.resize(n,vector<int>(m));
+    for(int i = 0;i<n;i++)
+    {
+        for(int j = 0 ;j<m;j++)
+            cin>>x[i][j];
+    }
+}
+void read(int n,vector<vector<int>>& x)
+{
+    x.clear();
+    x.resize(n+1);
+    for(int i = 0;i<n-1;i++)
+    {
+         int a,b;
+         cin>>a>>b;
+         x[a].pb(b);
+         x[b].pb(a);
+    }
+}
+void read(int n,vector<vector<int>>& x,int m)
+{
+    x.clear();
+    x.resize(n+1);
+    for(int i = 0;i<m;i++)
+    {
+         int a,b;
+         cin>>a>>b;
+         x[a].pb(b);
+         x[b].pb(a);
+    }
+}
+void read(int n,vector<ll>& x)
+{
+    x.clear();
+    x.resize(n);
+    for(int i = 0;i<n;i++)
+    {
+        cin>>x[i];
+    }
+}
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    int T = 1;
+    cin>>T;
+    for(int t = 1;t<=T;t++)
+    {
+        string s;
+        cin>>s;
+        stack<char> st;
+        for(int i = 0;i<s.size();i++)
+        {
+        	if(s[i] == 'A')
+        	{
+        		st.push('A');
+        	}
+        	else
+        	{
+        		if(!st.empty() && st.top() == 'A')
+        		{
+        			st.pop();
+        		}
+        		else
+        			st.push('B');
+        	}
+        }
+        vector<char> v;
+        while(!st.empty())
+        {
+        	if(!v.empty() && v.back() == 'B' && st.top() == 'B')
+        		v.pop_back();
+        	else
+        		v.push_back(st.top());
+        	st.pop();
+        }
+        cout<<v.size()<<endl;
+    }
+
+    return 0;
+}
