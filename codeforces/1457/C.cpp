2ed5bcbc2a7c296b6780d6fc912ad0ecf7a2818e
Kalash Shah <kalashshah11@gmail.com>
2020-11-30 14:40:00 +0530

    Add solution for problem `C - Bouncing Ball` Link: https://codeforces.com/contest/1457/problem/C Tags: dp, implementation, *1400 Ref: https://codeforces.com/contest/1457/submission/99967677

---
+++ codeforces/1457/C.cpp
@@ -0,0 +1,133 @@
+#include <bits/stdc++.h>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstring>
+#include <chrono>
+#include <complex>
+#define REP(i,a,b) for (auto i = a; i != b; i++)
+#define maxheap priority_queue < ll, std::vector<ll>, std::less<ll> >
+#define minheap priority_queue < ll, std::vector<ll>, std::greater<ll> >   // mxheap.push(), mxheap.top(), mxheap.pop()
+#define ll long long int
+#define ld long double
+#define vi vector<int>
+#define vll vector<ll>
+#define vvi vector < vi >
+#define pii pair<int,int>
+#define pll pair<long long, long long>
+#define mod 1000000007
+#define inf 1000000000000000001
+#define all(c) c.begin(),c.end()
+#define mp(x,y) make_pair(x,y)
+#define mem(a,val) memset(a,val,sizeof(a))
+#define eb emplace_back
+#define f first
+#define s second
+#define pb push_back
+#define SQ(a) (a)*(a)
+ 
+using namespace std;
+// using namespace FastIO;
+void read(int n,vector<int>& x)
+{
+    x.clear();
+    x.resize(n);
+    for(int i = 0;i<n;i++)
+    {
+        cin>>x[i];
+    }
+    
+}
+void read(int n,int m,vector<vector<int>>& x)
+{
+    x.clear();
+    x.resize(n,vector<int>(m));
+    for(int i = 0;i<n;i++)
+    {
+        for(int j = 0 ;j<m;j++)
+            cin>>x[i][j];
+    }
+}
+void read(int n,vector<vector<int>>& x)
+{
+    x.clear();
+    x.resize(n+1);
+    for(int i = 0;i<n-1;i++)
+    {
+         int a,b;
+         cin>>a>>b;
+         x[a].pb(b);
+         x[b].pb(a);
+    }
+}
+void read(int n,vector<vector<int>>& x,int m)
+{
+    x.clear();
+    x.resize(n+1);
+    for(int i = 0;i<m;i++)
+    {
+         int a,b;
+         cin>>a>>b;
+         x[a].pb(b);
+         x[b].pb(a);
+    }
+}
+void read(int n,vector<ll>& x)
+{
+    x.clear();
+    x.resize(n);
+    for(int i = 0;i<n;i++)
+    {
+        cin>>x[i];
+    }
+}
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    int T = 1;
+    // freopen("mixmilk.in", "r", stdin);
+    // freopen("mixmilk.out", "w", stdout);
+    cin>>T;
+    for(int t = 1;t<=T;t++)
+    {
+        // cout<<"Case #"<<t<<": ";
+        int n,p,k;
+        cin>>n>>p>>k;
+        string s;
+        cin>>s;
+        unordered_map<int,queue<int>> umap;
+        unordered_map<int,int> count;
+        unordered_map<int,int> size;
+        for(int i = p-1;i<n;i++)
+        {
+            if(s[i] == '1')
+            {
+                umap[i%k].push(i);
+                count[i%k]++;
+            }
+            else
+            {
+                umap[i%k].push(i);
+            }
+            size[i%k]++;
+        }
+        ll x,y;
+        cin>>x>>y;
+        ll ans = 10000000000000;
+        for(int i = p-1;i<n;i++)
+        {
+            
+            ll z = size[i%k] - count[i%k];
+            ans = min(ans,(i-p+1)*y + z*x);
+            int temp = umap[i%k].front();
+            umap[i%k].pop();
+            size[i%k]--;
+            if(s[temp] == '1')
+                count[i%k]--;
+        }
+        cout<<ans<<endl;
+    }
+
+    return 0;
+}
+
