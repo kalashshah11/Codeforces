16c874c4f383ecb0263d425c9dbb04738f6366f7
Kalash Shah <kalashshah11@gmail.com>
2020-12-09 18:31:00 +0530

    Add solution for problem `B - DZY Loves Chemistry` Link: https://codeforces.com/contest/445/problem/B Tags: dfs and similar, dsu, greedy, *1400 Ref: https://codeforces.com/contest/445/submission/100765412

--- README.md
+++ README.md
@@ -12,11 +12,12 @@ A repository to keep track of problem solving practise, containing solutions fro
 
 | # | Title | Solution | Tags | Submitted |
 |---| ----- | -------- | ---- | --------- |
-8 | [E1 - Close Tuples (easy version)](https://codeforces.com/contest/1462/problem/E1) | [GNU C++17](./codeforces\1462\E1.cpp) | `binary search` `combinatorics` `math` `sortings` `two pointers` `*1500` | Dec/15/2020 21:25 | 
-7 | [C - Unique Number](https://codeforces.com/contest/1462/problem/C) | [GNU C++17](./codeforces\1462\C.cpp) | `brute force` `greedy` `math` `*900` | Dec/15/2020 20:22 | 
-6 | [B - Last Year's Substring](https://codeforces.com/contest/1462/problem/B) | [GNU C++17](./codeforces\1462\B.cpp) | `dp` `implementation` `strings` `*800` | Dec/15/2020 20:19 | 
-5 | [A - Favorite Sequence](https://codeforces.com/contest/1462/problem/A) | [GNU C++17](./codeforces\1462\A.cpp) | `implementation` `two pointers` `*800` | Dec/15/2020 20:08 | 
-4 | [D - Divide and Summarize](https://codeforces.com/contest/1461/problem/D) | [GNU C++17](./codeforces\1461\D.cpp) | `binary search` `brute force` `data structures` `divide and conquer` `implementation` `sortings` `*1600` | Dec/11/2020 21:38 | 
-3 | [B - Find the Spruce](https://codeforces.com/contest/1461/problem/B) | [GNU C++17](./codeforces\1461\B.cpp) | `brute force` `dp` `implementation` `*1400` | Dec/11/2020 20:44 | 
-2 | [C - Random Events](https://codeforces.com/contest/1461/problem/C) | [GNU C++17](./codeforces\1461\C.cpp) | `dp` `math` `probabilities` `*1500` | Dec/11/2020 20:41 | 
-1 | [A - String Generation](https://codeforces.com/contest/1461/problem/A) | [GNU C++17](./codeforces\1461\A.cpp) | `constructive algorithms` `greedy` `*800` | Dec/11/2020 20:07 | 
+9 | [E1 - Close Tuples (easy version)](https://codeforces.com/contest/1462/problem/E1) | [GNU C++17](./codeforces\1462\E1.cpp) | `binary search` `combinatorics` `math` `sortings` `two pointers` `*1500` | Dec/15/2020 21:25 | 
+8 | [C - Unique Number](https://codeforces.com/contest/1462/problem/C) | [GNU C++17](./codeforces\1462\C.cpp) | `brute force` `greedy` `math` `*900` | Dec/15/2020 20:22 | 
+7 | [B - Last Year's Substring](https://codeforces.com/contest/1462/problem/B) | [GNU C++17](./codeforces\1462\B.cpp) | `dp` `implementation` `strings` `*800` | Dec/15/2020 20:19 | 
+6 | [A - Favorite Sequence](https://codeforces.com/contest/1462/problem/A) | [GNU C++17](./codeforces\1462\A.cpp) | `implementation` `two pointers` `*800` | Dec/15/2020 20:08 | 
+5 | [D - Divide and Summarize](https://codeforces.com/contest/1461/problem/D) | [GNU C++17](./codeforces\1461\D.cpp) | `binary search` `brute force` `data structures` `divide and conquer` `implementation` `sortings` `*1600` | Dec/11/2020 21:38 | 
+4 | [B - Find the Spruce](https://codeforces.com/contest/1461/problem/B) | [GNU C++17](./codeforces\1461\B.cpp) | `brute force` `dp` `implementation` `*1400` | Dec/11/2020 20:44 | 
+3 | [C - Random Events](https://codeforces.com/contest/1461/problem/C) | [GNU C++17](./codeforces\1461\C.cpp) | `dp` `math` `probabilities` `*1500` | Dec/11/2020 20:41 | 
+2 | [A - String Generation](https://codeforces.com/contest/1461/problem/A) | [GNU C++17](./codeforces\1461\A.cpp) | `constructive algorithms` `greedy` `*800` | Dec/11/2020 20:07 | 
+1 | [B - DZY Loves Chemistry](https://codeforces.com/contest/445/problem/B) | [GNU C++17](./codeforces\445\B.cpp) | `dfs and similar` `dsu` `greedy` `*1400` | Dec/09/2020 18:31 | 
---
+++ codeforces/445/B.cpp
@@ -0,0 +1,178 @@
+#include <bits/stdc++.h>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstring>
+#include <chrono>
+#include <complex>
+#define REP(i,a,b) for (auto i = a; i != b; i++)
+#define ll long long int
+#define ld long double
+#define vi vector<int>
+#define vll vector<ll>
+#define vvi vector < vi >
+#define pii pair<int,int>
+#define pll pair<long long, long long>
+#define mod 1000000007
+#define inf 1000000000000000001
+#define all(c) c.begin(),c.end()
+#define mp(x,y) make_pair(x,y)
+#define mem(a,val) memset(a,val,sizeof(a))
+#define eb emplace_back
+#define f first
+#define s second
+#define pb push_back
+#define SQ(a) (a)*(a)
+ 
+using namespace std;
+
+void read(int n,vector<int>& x)
+{
+    x.clear();
+    x.resize(n);
+    for(int i = 0;i<n;i++)
+    {
+        cin>>x[i];
+    }
+}
+void read(int n,int m,vector<vector<int>>& x)
+{
+    x.clear();
+    x.resize(n,vector<int>(m));
+    for(int i = 0;i<n;i++)
+    {
+        for(int j = 0 ;j<m;j++)
+            cin>>x[i][j];
+    }
+}
+void read(int n,vector<vector<int>>& x)
+{
+    x.clear();
+    x.resize(n+1);
+    for(int i = 0;i<n-1;i++)
+    {
+         int a,b;
+         cin>>a>>b;
+         x[a].pb(b);
+         x[b].pb(a);
+    }
+}
+void read(int n,vector<vector<int>>& x,int m)
+{
+    x.clear();
+    x.resize(n+1);
+    for(int i = 0;i<m;i++)
+    {
+         int a,b;
+         cin>>a>>b;
+         x[a].pb(b);
+         x[b].pb(a);
+    }
+}
+void read(int n,vector<ll>& x)
+{
+    x.clear();
+    x.resize(n);
+    for(int i = 0;i<n;i++)
+    {
+        cin>>x[i];
+    }
+}
+struct DSU{
+    int tsize;
+    vector<ll> parent;
+    vector<ll> rank;
+    vector<ll> size;
+    ll maxsize = 1;
+    int components;
+    void init(int n)
+    {
+        tsize = n;
+        parent.resize(n+1);
+        rank.resize(n+1,0);
+        size.resize(n+1,1);
+        for(int i =1 ;i<=n;i++)
+        {
+            parent[i] = i;
+        }   
+        components = n;
+    }
+    ll getParent(ll a) {
+        return parent[a] = (parent[a] == a ? a : getParent(parent[a]));
+    }
+    int doUnion(ll newa, ll newb) {
+        ll a = getParent(newa);
+        ll b = getParent(newb);
+        if (a != b) {
+
+            if(rank[a] < rank[b])
+            {
+                ll temp = a;
+                a = b;
+                b = temp;
+            }
+            if(rank[a] == rank[b])
+            {
+                rank[a] += 1;
+            }
+            parent[b] = a;
+            size[a] += size[b];
+            maxsize = max(maxsize,size[a]);
+            size[b] = 0;
+            components--;
+            return 1;
+        } 
+        else
+            return 0;  
+    }
+    ll getMaxSize()
+    {
+        return maxsize;
+    }
+    ll getComponents()
+    {
+        return components;
+    }
+};
+ll power(ll a, ll b)	//a is base, b is exponent
+{
+    if(b==0)
+        return 1;
+    if(b==1)
+        return a;
+    if(b%2 == 1)
+        return (power(a,b-1)*a);
+    ll q = power(a,b/2);
+    return (q*q);
+}
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    int T = 1;
+    // cin>>T;
+    //segmentedSieve(1000000000LL);
+    for(int t = 1;t<=T;t++)
+    {
+        int n,m;
+        cin>>n>>m;
+        DSU mydsu;
+        // cout<<n<<d<<" ";
+        mydsu.init(n);
+        for(int i = 0;i<m;i++)
+        {
+            int x,y;
+            cin>>x>>y;
+            mydsu.doUnion(x,y);
+        }
+        cout<<power(2,n-mydsu.getComponents())<<endl;
+    }
+
+    return 0;
+}
+    
+
+
+    
+    
+    
